18f16a686af22283e0a708b56823e9b795636b2e
Xucong Zhan <zhanxucong@gmail.com>
2022-02-03 16:47:53 -0500

    feat: added project list

---
+++ src/screens/project-list/Index.jsx
@@ -0,0 +1,40 @@
+import { useEffect, useState } from "react";
+import { List } from "./List";
+import { SearchPanel } from "./SearchPanel";
+import { stringify } from "qs";
+import { cleanObject } from "../../utils";
+
+const apiUrl = process.env.REACT_APP_API_URL;
+
+export const ProjectListScreen = () => {
+  const [users, setUsers] = useState([]);
+  const [param, setParam] = useState({
+    name: "",
+    personId: "",
+  });
+  const [list, setList] = useState([]);
+
+  useEffect(() => {
+    fetch(`${apiUrl}/projects?${stringify(cleanObject(param))}`).then(
+      async (response) => {
+        if (response.ok) {
+          setList(await response.json());
+        }
+      }
+    );
+  }, [param]);
+  useEffect(() => {
+    fetch(`${apiUrl}/users`).then(async (response) => {
+      if (response.ok) {
+        setUsers(await response.json());
+      }
+    });
+  }, []);
+
+  return (
+    <div>
+      <SearchPanel param={param} setParam={setParam} users={users} />
+      <List list={list} users={users} />
+    </div>
+  );
+};
