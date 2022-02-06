a8749dd419ea839b7fdd407bb8dd3cd5aa0314c0
-g <-g>
2022-02-05 19:26:23 -0500

    feat: added LoginScreen and login middleware

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
+import { LoginScreen } from "screens/login/Index";
 import { ProjectListScreen } from "./screens/project-list/Index";
 
 function App() {
   return (
     <div className="App">
-      <ProjectListScreen />
+      <LoginScreen />
+      {/* <ProjectListScreen /> */}
     </div>
   );
 }
