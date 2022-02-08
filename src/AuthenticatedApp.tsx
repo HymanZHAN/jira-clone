bf9cb9848fd8836df0292709a39dc9c4739eba82
Xucong Zhan <zhanxucong@gmail.com>
2022-02-08 16:16:22 -0500

    style: 用Row布局页面

--- src/AuthenticatedApp.tsx
+++ src/AuthenticatedApp.tsx
@@ -1,5 +1,6 @@
 import styled from "@emotion/styled";
 import { Button } from "antd";
+import { Row } from "components/lib";
 import { useAuth } from "contexts/AuthContext";
 import { ProjectListScreen } from "screens/project-list/Index";
 
@@ -8,11 +9,11 @@ export const AuthenticatedApp = () => {
   return (
     <div>
       <Container>
-        <Header>
-          <HeaderLeft>
-            <h3>Logo</h3>
-            <h3>项目</h3>
-            <h3>用户</h3>
+        <Header between={true}>
+          <HeaderLeft gap={true}>
+            <h2>Logo</h2>
+            <h2>项目</h2>
+            <h2>用户</h2>
           </HeaderLeft>
           <HeaderRight>
             <Button onClick={logout}>登出</Button>
@@ -30,17 +31,13 @@ const Container = styled.div`
   height: 100vh;
 `;
 
-const Header = styled.header`
+const Header = styled(Row)`
   height: 6rem;
   box-shadow: rgba(0, 0, 0, 0.1) 0 0 10px;
-  display: flex;
-  flex-direction: row;
-  align-items: center;
-  justify-content: space-between;
 `;
-const HeaderLeft = styled.div`
-  display: flex;
-  align-items: center;
-`;
+// const HeaderItem = styled.h3`
+//   margin-right: 3rem;
+// `;
+const HeaderLeft = styled(Row)``;
 const HeaderRight = styled.div``;
 const Main = styled.main``;
---
+++ src/components/lib.tsx
@@ -0,0 +1,19 @@
+import styled from "@emotion/styled";
+
+export const Row = styled.div<{
+  gap?: number | boolean;
+  between?: boolean;
+  marginBottom?: number;
+}>`
+  display: flex;
+  align-items: center;
+  justify-content: ${({ between }) => (between ? "space-between" : undefined)};
+  margin-bottom: ${({ marginBottom }) =>
+    marginBottom ? marginBottom + "rem" : undefined};
+  > * {
+    margin-top: 0 !important;
+    margin-bottom: 0 !important;
+    margin-right: ${({ gap }) =>
+      typeof gap === "number" ? gap + "rem" : gap ? "2rem" : undefined};
+  }
+`;
